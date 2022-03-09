fde406cd42dc8b5941f61cd76d362c393b395502
muhammad abubakar <mabubakarbssg@gmail.com>
2022-03-09 08:23:24 -0500

    api_code instead of api code

---
+++ API_code.py
@@ -0,0 +1,19 @@
+#from typing import Optional
+#from fastapi import FastAPI
+
+from flask import Flask
+import factorialcode
+
+app = Flask(__name__)
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.route('/factorial/<int:num>/')
+def read_item(num: int):
+    fact = factorialcode.factorial(num)
+    return {"factorial": fact}
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002, debug=True)
